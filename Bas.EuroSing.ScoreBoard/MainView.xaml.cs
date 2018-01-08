c1e7eadfdfd9872214beecf6f8b551c278cc4148
Bas Paap <baspaap@live.nl>
2018-01-08 19:23:46 +0100

    MainView toegevoegd en MainWindow verwijderd.

--- Bas.EuroSing.ScoreBoard/MainView.xaml.cs
+++ Bas.EuroSing.ScoreBoard/MainView.xaml.cs
@@ -18,9 +18,9 @@ namespace Bas.EuroSing.ScoreBoard
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : Window
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
         }
---
+++ Bas.EuroSing.ScoreBoard/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bas.EuroSing.ScoreBoard.ViewModels
+{
+    public sealed class MainViewModel : ViewModelBase
+    {
+    }
+}
