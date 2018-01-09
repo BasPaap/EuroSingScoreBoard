36a131346b30b59a00ed5bd4d2bc614728263ed9
Bas Paap <bpaap@debibliotheekaanzet.nl>
2018-01-09 12:54:57 +0100

    Unieke indexes op Value-FromCountryId en FromCountryId-ToCountryId.

--- Bas.EuroSing.ScoreBoard/Model/Points.cs
+++ Bas.EuroSing.ScoreBoard/Model/Points.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,16 @@ namespace Bas.EuroSing.ScoreBoard.Model
     public class Points
     {
         public int Id { get; set; }
+
+        [Index("IX_ValueAndFromCountryId", 1, IsUnique = true)]
         public int Value { get; set; }
 
+        [Index("IX_ValueAndFromCountryId", 2, IsUnique = true)]
+        [Index("IX_FromCountryIdAndToCountryId", 1, IsUnique = true)]
         public int FromCountryId { get; set; }
         public virtual Country FromCountry { get; set; }
 
+        [Index("IX_FromCountryIdAndToCountryId", 1, IsUnique = true)]
         public int ToCountryId { get; set; }
         public virtual Country ToCountry { get; set; }
     }
