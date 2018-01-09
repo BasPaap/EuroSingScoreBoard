01283af899078553c8917b110751e600a7dea996
Bas Paap <bpaap@debibliotheekaanzet.nl>
2018-01-09 13:30:45 +0100

    Drag & Drop deels geimplementeerd

--- Bas.EuroSing.ScoreBoard/ViewModels/SettingsViewModel.cs
+++ Bas.EuroSing.ScoreBoard/ViewModels/SettingsViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using Bas.EuroSing.ScoreBoard.Services;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Bas.EuroSing.ScoreBoard.ViewModels
 {
@@ -13,11 +16,26 @@ namespace Bas.EuroSing.ScoreBoard.ViewModels
     {
         public ObservableCollection<CountryListItemViewModel> Countries { get; set; }
 
+        public RelayCommand<DragEventArgs> DropCommand { get; set; }
+        
         public SettingsViewModel(IDataService dataService)
         {
+            DropCommand = new RelayCommand<DragEventArgs>(OnDropCommand);
+
             Countries = new ObservableCollection<CountryListItemViewModel>(from c in dataService.GetAllCountries()
                                                                            orderby c.Name
                                                                            select new CountryListItemViewModel() { Name = c.Name });
+        }
+
+        private void OnDropCommand(DragEventArgs e)
+        {
+            var filePaths = e.Data.GetData(DataFormats.FileDrop) as IEnumerable<string>;
+
+            foreach (var filePath in filePaths)
+            {
+                var countryName = Path.GetFileNameWithoutExtension(filePath);
+                Countries.Insert(1, new CountryListItemViewModel() { Name = countryName });
+            }
         }
     }
 }
