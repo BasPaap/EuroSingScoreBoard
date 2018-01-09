1ab97e0b14b98f7a6f2408d531e6955771ef3931
Bas Paap <bpaap@debibliotheekaanzet.nl>
2018-01-09 23:38:49 +0100

    Combobox met aparte stijl toegevoegd.

--- Bas.EuroSing.ScoreBoard/ViewModels/VoteViewModel.cs
+++ Bas.EuroSing.ScoreBoard/ViewModels/VoteViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using Bas.EuroSing.ScoreBoard.Messages;
+using Bas.EuroSing.ScoreBoard.Services;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +14,23 @@ namespace Bas.EuroSing.ScoreBoard.ViewModels
 {
     internal class VoteViewModel : ViewModelBase
     {
+        private IDataService dataService;
+
         public RelayCommand SettingsCommand { get; set; }
 
-        public VoteViewModel()
+
+        public ObservableCollection<CountryListItemViewModel> Countries { get; set; }
+
+
+        public VoteViewModel(IDataService dataService)
         {
+            this.dataService = dataService;
+
             SettingsCommand = new RelayCommand(() => MessengerInstance.Send(new GenericMessage<Message>(Message.ShowSettings)));
+
+            Countries = new ObservableCollection<CountryListItemViewModel>(from c in dataService.GetAllCountries()
+                                                                           orderby c.Name
+                                                                           select new CountryListItemViewModel(c, this.dataService));
         }
     }
 }
