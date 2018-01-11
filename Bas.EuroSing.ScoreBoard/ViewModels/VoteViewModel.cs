3e89bceefbbabae96a8af0df9bcb007edfc8ffe5
Bas Paap <bpaap@debibliotheekaanzet.nl>
2018-01-11 16:18:42 +0100

    ShowResultsButton werkt nu alleen als alle landen gestemd hebben.

--- Bas.EuroSing.ScoreBoard/ViewModels/VoteViewModel.cs
+++ Bas.EuroSing.ScoreBoard/ViewModels/VoteViewModel.cs
@@ -16,6 +16,7 @@ using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Bas.EuroSing.ScoreBoard.ViewModels
 {
@@ -25,9 +26,18 @@ namespace Bas.EuroSing.ScoreBoard.ViewModels
         private List<int> validPointValues = new List<int>(new int[] { 12, 10, 8, 7, 6, 5, 4, 3, 2, 1 });
 
         public RelayCommand SettingsCommand { get; set; }
+        public RelayCommand ShowResultsCommand { get; set; }
 
         public ObservableCollection<CountryListItemViewModel> Countries { get; set; }
         public ObservableCollection<int> VotesToCast { get; set; }
+        
+        private float showResultsButtonOpacity;
+
+        public float ShowResultsButtonOpacity
+        {
+            get { return showResultsButtonOpacity; }
+            set { Set(ref showResultsButtonOpacity, value); }
+        }
 
         private CountryListItemViewModel countryIssuingVotes;
 
@@ -95,7 +105,7 @@ namespace Bas.EuroSing.ScoreBoard.ViewModels
             VotesToCast = new ObservableCollection<int>();
 
             SettingsCommand = new RelayCommand(() => MessengerInstance.Send(new GenericMessage<Message>(Message.ShowSettings)));
-
+            ShowResultsCommand = new RelayCommand(OnShowResultsCommand);
             UpdateCountries();
 
             Messenger.Default.Register<CountriesUpdatedMessage>(this, (m) =>
@@ -110,7 +120,8 @@ namespace Bas.EuroSing.ScoreBoard.ViewModels
             {
                 PopulateVotesToCast();
 
-                CountryIssuingVotes.IsComplete = (VotesToCast.Count == 0);                
+                CountryIssuingVotes.IsComplete = (VotesToCast.Count == 0);
+                UpdateShowResultsButton();
             });
 
             if (ViewModelBase.IsInDesignModeStatic)
@@ -119,7 +130,39 @@ namespace Bas.EuroSing.ScoreBoard.ViewModels
                 UpdateCountriesToVoteOn();
             }
         }
-        
+
+        private void OnShowResultsCommand()
+        {
+            if (AreAllCountriesComplete())
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Not all votes have been cast yet!", "EuroSing 2018", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+
+        private void UpdateShowResultsButton()
+        {         
+            ShowResultsButtonOpacity = (AreAllCountriesComplete()) ? 1.0f : 0.2f;
+        }
+
+        private bool AreAllCountriesComplete()
+        {
+            bool allCountriesAreComplete = true;
+
+            foreach (var country in Countries)
+            {
+                if (!country.IsComplete)
+                {
+                    allCountriesAreComplete = false;
+                }
+            }
+
+            return allCountriesAreComplete;
+        }
+
         private void UpdateCountries()
         {
             Countries.Clear();
@@ -130,10 +173,11 @@ namespace Bas.EuroSing.ScoreBoard.ViewModels
 
             foreach (var country in allCountries)
             {                
-                Countries.Add(country);
+                Countries.Add(country);                
             }
 
             CountryIssuingVotes = null;
+            UpdateShowResultsButton();
         }
 
     }
