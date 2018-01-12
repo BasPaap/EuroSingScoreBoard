b0aa16ce858c999e2f219ecba239c40c4b8bdfa7
Bas Paap <bpaap@debibliotheekaanzet.nl>
2018-01-13 00:06:03 +0100

    ResultsViewModel aangepast.

--- Bas.EuroSing.ScoreBoard/ViewModels/ResultsViewModel.cs
+++ Bas.EuroSing.ScoreBoard/ViewModels/ResultsViewModel.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Bas.EuroSing.ScoreBoard.Services;
+using GalaSoft.MvvmLight;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bas.EuroSing.ScoreBoard.ViewModels
 {
-    class ResultsViewModel
+    internal class ResultsViewModel : ViewModelBase
     {
+        private IDataService dataService;
+        public ObservableCollection<CountryListItemViewModel> Countries { get; set; }
+
+        public ResultsViewModel(IDataService dataService)
+        {
+            this.dataService = dataService;
+
+            Countries = new ObservableCollection<CountryListItemViewModel>(from c in dataService.GetAllCountries()
+                                                                           orderby c.Name
+                                                                           select new CountryListItemViewModel(c, this.dataService));
+        }
+
     }
 }
